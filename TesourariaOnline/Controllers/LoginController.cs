01dd00b0c969b2ff9d3b4d65fa682bcceeca17e3
allan6slip <allan6slip@hotmail.com>
2019-05-06 13:46:23 -0300

    Created login Controller

---
+++ TesourariaOnline/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TesourariaOnline.Models;
+
+namespace TesourariaOnline.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly TesourariaOnlineContext _context;
+        [HttpGet]
+        public IActionResult Login(string userName,string password)
+        {
+            try
+            {
+                _context.
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View();
+        }
+    }
+}
